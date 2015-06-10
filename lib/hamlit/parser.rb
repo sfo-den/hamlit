haml/haml

commit 06ee81a2f8dd89291c40fa2198d922e73ddc7f9a

Support & operator and unify behavior of !

lib/hamlit/parser.rb
@@ -102,13 +102,18 @@ module Hamlit
       return parse_text(scanner, lstrip: true, escape: false) if scanner.scan(/!( |==)/)
       return parse_script(scanner, force_escape: true)        if scanner.match?(/&=/)
       return parse_script(scanner, disable_escape: true)      if scanner.match?(/!=/)
-      return parse_text(scanner, lstrip: true, escape: false) if scanner.scan(/!/)
 
       case scanner.peek(1)
       when '=', '~'
         parse_script(scanner)
       when '-'
         parse_silent_script(scanner)
+      when '!'
+        scanner.scan(/!/)
+        parse_text(scanner, lstrip: true, escape: false)
+      when '&'
+        scanner.scan(/&/)
+        parse_text(scanner, lstrip: true, escape: true)
       else
         parse_text(scanner)
       end

spec/hamlit/engine/text_spec.rb
@@ -72,6 +72,44 @@ describe Hamlit::Engine do
     end
 
     describe 'inline operator' do
+      it 'render ! operator' do
+        assert_render(<<-'HAML', <<-'HTML')
+          %span!#{'<nyaa>'}
+          %span! #{'<nyaa>'}
+          ! #{'<nyaa>'}
+        HAML
+          <span><nyaa></span>
+          <span><nyaa></span>
+          <nyaa>
+        HTML
+      end
+
+      it 'render & operator' do
+        assert_render(<<-'HAML', <<-'HTML')
+          %span& #{'<nyaa>'}
+          %span&#{'<nyaa>'}
+          & #{'<nyaa>'}
+        HAML
+          <span>&lt;nyaa&gt;</span>
+          <span>&lt;nyaa&gt;</span>
+          &lt;nyaa&gt;
+        HTML
+      end
+
+      it 'render !, & operator right before a non-space character' do
+        assert_render(<<-'HAML', <<-'HTML', compatible_only: :faml)
+          &nbsp;
+          \&nbsp;
+          !hello
+          \!hello
+        HAML
+          nbsp;
+          &nbsp;
+          hello
+          !hello
+        HTML
+      end
+
       it 'does not accept backslash operator' do
         assert_render(<<-'HAML', <<-'HTML')
           %span\    foo
@@ -107,20 +145,6 @@ describe Hamlit::Engine do
           %span&= '<nyaa>'
         HAML
           <span>&lt;nyaa&gt;</span>
-        HTML
-      end
-
-      it 'accepts ! operator' do
-        assert_render(<<-'HAML', <<-'HTML', compatible_only: :faml)
-          %span!#{'<nyaa>'}
-          %span! #{'<nyaa>'}
-          !#{'<nyaa>'}
-          ! #{'<nyaa>'}
-        HAML
-          <span><nyaa></span>
-          <span><nyaa></span>
-          <nyaa>
-          <nyaa>
         HTML
       end
 

sfo-den/hamlit

commit 1d4f1576078b551efb9672d93a970a9763b7799a

Fix HTML entities rendering result
Fixes #27.

This is the known regression by
06ee81a2f8dd89291c40fa2198d922e73ddc7f9a.
It was intended to imitate the behavior of faml.
But may be this change and the inconsistent behavior is more convenient
to write HTML entities.

lib/hamlit/parser.rb
@@ -76,7 +76,7 @@ module Hamlit
         return ast if ast
       end
 
-      parse_inner_line(scanner)
+      parse_inner_line(scanner, inline: inline)
     end
 
     # Parse a line and return ast if it is acceptable outside an inline tag
@@ -100,20 +100,21 @@ module Hamlit
     end
 
     # Parse a line and return ast which is acceptable inside an inline tag
-    def parse_inner_line(scanner)
+    def parse_inner_line(scanner, inline: false)
       return parse_text(scanner, lstrip: true)                if scanner.scan(/==/)
-      return parse_text(scanner, lstrip: true, escape: false) if scanner.scan(/!==/)
-      return parse_text(scanner, lstrip: true, escape: true)  if scanner.scan(/&==/)
+      return parse_text(scanner, lstrip: true, escape: false) if scanner.scan(/! |!==/)
+      return parse_text(scanner, lstrip: true, escape: true)  if scanner.scan(/& |&==/)
       return parse_script(scanner, disable_escape: true)      if scanner.match?(/!=/)
       return parse_script(scanner, force_escape: true)        if scanner.match?(/&=/)
+
+      if inline
+        return parse_text(scanner, lstrip: true, escape: false) if scanner.scan(/!/)
+        return parse_text(scanner, lstrip: true, escape: true)  if scanner.scan(/&/)
+      end
 
       case scanner.peek(1)
       when '=', '~'
         parse_script(scanner)
-      when '!'
-        parse_text(scanner, lstrip: true, escape: false, scan: /!/)
-      when '&'
-        parse_text(scanner, lstrip: true, escape: true, scan: /&/)
       else
         parse_text(scanner)
       end

spec/hamlit/engine/text_spec.rb
@@ -116,15 +116,15 @@ describe Hamlit::Engine do
       end
 
       it 'renders !, & operator right before a non-space character' do
-        assert_render(<<-'HAML', <<-'HTML', compatible_only: :faml)
+        assert_render(<<-'HAML', <<-'HTML', compatible_only: :haml)
           &nbsp;
           \&nbsp;
           !hello
           \!hello
         HAML
-          nbsp;
           &nbsp;
-          hello
+          &nbsp;
+          !hello
           !hello
         HTML
       end